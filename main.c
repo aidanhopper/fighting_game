1dc84b2506fe885d39ab86bef7def868dd450185
Aidan Hopper <aidanhopper@pop-os.localdomain>
2023-11-22 16:22:51 -0800

    rectangle

--- main.c
+++ main.c
@@ -52,9 +52,15 @@ int main() {
   glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
   float vertices[] = {
-      -0.5f, -0.5f, 0.0f, // left
-      0.5f,  -0.3f, 0.0f, // right
-      0.0f,  0.5f,  0.0f  // top
+      0.5f,  0.5f,  0.0f, // top right
+      0.5f,  -0.5f, 0.0f, // bottom right
+      -0.5f, -0.5f, 0.0f, // bottom left
+      -0.5f, 0.5f,  0.0f  // top left
+  };
+  unsigned int indices[] = {
+      // note that we start from 0!
+      0, 1, 3, // first triangle
+      1, 2, 3  // second triangle
   };
 
   // Shader stuff
@@ -70,12 +76,15 @@ int main() {
   glLinkProgram(shaderProgram);
 
   // idk
-  u32 VAO, VBO;
+  u32 VAO, VBO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
   glBindVertexArray(VAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
@@ -100,7 +109,7 @@ int main() {
 
     // draw
     glColor3f(0.0, 0.0, 0.0);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     SDL_GL_SwapWindow(state.window);
   }
